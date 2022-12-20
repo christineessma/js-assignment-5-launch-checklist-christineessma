87f0c44a4eda432834c6445e2e99dba39f8ced37
christineessma <christine.essma@gmail.com>
2022-12-19 19:13:55 -0600

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Write your JavaScript code here!
 
+const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
@@ -13,4 +15,20 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+   let form = document.querySelector("form")
+   form.addEventListener("submit", function (event) {
+    
+    //event.preventDefault()
+    let pilot = document.querySelector("input [name=pilotName]")
+    let pilotValue = pilot.value
+    let copilot = document.querySelector("input [name=copilotName]")
+    let copilotValue = copilot.Value
+    let fuel = document.querySelector("input [name=fuelLevel]")
+    let fuelValue = fuel.Value
+    let cargo = document.querySelector("input [name=cargoMass]")
+    let cargoValue = cargo.value
+   
+    event.preventDefault()
+    formSubmission(documtent, list, pilotValue, copilotValue, fuelValue, cargoValue);
+   })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -14,15 +14,59 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+  let div = document.getElementById("missionTarget")
+   div.innerHTML = 
+   <><h2>Mission Destination</h2><ol>
+           <li>Name: ${name}</li>
+           <li>Diameter: ${diameter}</li>
+           <li>Star: ${star}</li>
+           <li>Distance from Earth: ${distance}</li>
+           <li>Number of Moons: ${moons}</li>
+       </ol><img src="${imageUrl}"></img></>
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === "") {return "Empty"}
+   else if (typeof testInput == Number) {return "Is a Number"}
+   else {return "Not a Number"}
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    let launchStatus = document.getElementById("launchStatus");
 }
+
+if (fuelLevel < 10000 && cargoLevel > 10000 ) {
+    pilotStatus.innerHTML = 'Pilot ${pilot} ready for launch.';
+    copilotStatus.innerHTML = 'Copilot ${copilot} ready for launch.';
+    fuelStatus.innerHTML = "Fuel level too low for launch.";
+    cargoStatus.innerHTML = "Cargo mass too heavy for launch.";
+    launchStatus.innerHTML = "Shuttle not ready for launch.";
+} else if (fuelLevel < 10000 && cargoLevel <= 10000) {
+    pilotStatus.innerHTML = 'Pilot ${pilot} ready for launch.';
+    copilotStatus.innerHTML = 'Copilot ${copilot} ready for launch.';
+    fuelStatus.innerHTML = "Fuel level too low for launch.";
+    cargoStatus.innerHTML = "Cargo mass low enough for launch.";
+    launchStatus.innerHTML = "Shuttle not ready for launch.";
+} else if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+    pilotStatus.innerHTML = 'Pilot ${pilot} ready for launch.';
+    copilotStatus.innerHTML = 'Copilot ${copilot} ready for launch.';
+    fuelStatus.innerHTML = "Fuel level high enough for launch.";
+    cargoStatus.innerHTML = "Cargo mass too heavy for launch.";
+    launchStatus.innerHTML = "Shuttle not ready for launch."
+} else {
+    pilotStatus.innerHTML = 'Pilot ${pilot} ready for launch.';
+    copilotStatus.innerHTML = 'Copilot ${copilot} ready for launch.';
+    fuelStatus.innerHTML = "Fuel level high enough for launch.";
+    cargoStatus.innerHTML = "Cargo mass low enough for launch.";
+    launchStatus.innerHTML = "Shuttle ready for launch."
+}
+    
+
+
 
 async function myFetch() {
     let planetsReturned;
